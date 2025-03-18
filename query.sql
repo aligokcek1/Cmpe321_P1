3f223159784ed9cc28fe0e3c62e21391133f63a9
Ali GÖKÇEK <aligokcek1@gmail.com>
2025-03-18 16:57:31 +0300

    update query

--- query.sql
+++ query.sql
@@ -1,17 +1,22 @@
+CREATE DATABASE ChessDB;
+USE ChessDB;
+
 CREATE TABLE User (
 username VARCHAR(20) PRIMARY KEY,
 password VARCHAR(20),
 name VARCHAR(20),
 surname VARCHAR(20),
-nationality VARCHAR(20)
+nationality VARCHAR(20),
 
+PRIMARY KEY (username)
 );
 
 CREATE TABLE Player(
 username VARCHAR(20),
-elo_rating INT,
+elo_rating INT CHECK (elo_rating > 1000),
 date_of_birth DATE,
 fide_ID INT,
+-- teamlist will added
 
 PRIMARY KEY (username),
 FOREIGN KEY (username) REFERENCES User(username) ON DELETE CASCADE
