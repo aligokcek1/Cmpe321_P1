20741e8c15d760447941317cc3804b23c2e07429
Ali GÖKÇEK <aligokcek1@gmail.com>
2025-03-19 17:27:03 +0300

    update query er

--- query.sql
+++ query.sql
@@ -59,13 +59,13 @@ CREATE TABLE Hall(
 CREATE TABLE Tournament(
 	tournament_ID INT,
 	tournament_name VARCHAR(50) NOT NULL,
-	format VARCHAR(20) NOT NULL,
+	format VARCHAR(50) NOT NULL,
 	start_date DATE NOT NULL,
 	end_date DATE NOT NULL,
-	chief_arbiter VARCHAR(20) NOT NULL,
+	chief_arbiter VARCHAR(50) NOT NULL,
 
 	PRIMARY KEY (tournament_ID),
-	FOREIGN KEY (chief_arbiter) REFERENCES Arbiter(username)
+	FOREIGN KEY (chief_arbiter) REFERENCES Arbiter(username) ON DELETE CASCADE
 );
 
 -- `utilizes` relationship (tournament utilizes at least one hall)
@@ -85,38 +85,45 @@ CREATE TABLE ChessMatch(
 	time_slot INT NOT NULL CHECK (time_slot BETWEEN 1 AND 4),
 	rating INT NOT NULL CHECK (rating BETWEEN 1 AND 10),
 	black_player_team INT NOT NULL,
-	black_player VARCHAR(20) NOT NULL,
+	black_player VARCHAR(50) NOT NULL,
 	hall_ID INT NOT NULL,
 	result ENUM('white wins', 'black wins', 'draw') NOT NULL,
-	assigned_arbiter_username VARCHAR(20) NOT NULL,
+	assigned_arbiter_username VARCHAR(50) NOT NULL,
 	white_player_team INT NOT NULL,
-	white_player VARCHAR(20) NOT NULL,
+	white_player VARCHAR(50) NOT NULL,
 	table_ID INT NOT NULL,
 
 	PRIMARY KEY (match_ID),
-	FOREIGN KEY (tournament_ID) REFERENCES Tournament(tournament_ID),
-	FOREIGN KEY (hall_ID) REFERENCES Hall(hall_ID),
-	FOREIGN KEY (white_player) REFERENCES Player(username),
-	FOREIGN KEY (black_player) REFERENCES Player(username),
-	FOREIGN KEY (white_player_team) REFERENCES Team(team_ID),
-	FOREIGN KEY (black_player_team) REFERENCES Team(team_ID),
-	FOREIGN KEY (assigned_arbiter_username) REFERENCES Arbiter(username),
+    -- match should be deleted when a tournament is deleted
+	FOREIGN KEY (tournament_ID) REFERENCES Tournament(tournament_ID) ON DELETE CASCADE,
+	FOREIGN KEY (hall_ID) REFERENCES Hall(hall_ID) ON DELETE CASCADE,
+	FOREIGN KEY (white_player) REFERENCES Player(username) ON DELETE CASCADE,
+	FOREIGN KEY (black_player) REFERENCES Player(username) ON DELETE CASCADE,
+	FOREIGN KEY (white_player_team) REFERENCES Team(team_ID) ON DELETE CASCADE,
+	FOREIGN KEY (black_player_team) REFERENCES Team(team_ID) ON DELETE CASCADE,
+	FOREIGN KEY (assigned_arbiter_username) REFERENCES Arbiter(username) ON DELETE CASCADE,
     -- `played_in` key constraint & total participation 
     FOREIGN KEY (table_ID) REFERENCES MatchTable(table_ID) ON DELETE CASCADE,
 	-- Constraint to prevent same team players competing against each other
 	CONSTRAINT different_teams CHECK (white_player_team != black_player_team),
 	-- Unique constraint for hall, table, date, and time slot
-	CONSTRAINT unique_match_slot UNIQUE (hall_ID, table_ID, date, time_slot)
+	CONSTRAINT unique_match_slot UNIQUE (hall_ID, table_ID, date, time_slot),
+    -- Unique constraint to prevent arbiters managing more than one match at the same time
+    CONSTRAINT unique_arbiter_time UNIQUE (assigned_arbiter_username, date, time_slot),
+    -- A player cannot be assigned to matches with overlapping time_slots 
+    -- It is not complately correct for 2 time slot check since we cannot use trigggers
+    CONSTRAINT unique_white_player_time UNIQUE (white_player, date, time_slot),
+    CONSTRAINT unique_black_player_time UNIQUE (black_player, date, time_slot)
 );
 
--- `includes` relation. Tournament includes at least ChessMatch
+-- `includes` relation. Tournament includes at least one ChessMatch
 CREATE TABLE TournamentIncludesMatch(
-tournament_ID INT,
-match_ID INT NOT NULL,
- 
-PRIMARY KEY (tournament_ID, match_ID),
-FOREIGN KEY (tournament_ID) REFERENCES Tournament(tournament_ID) ON DELETE CASCADE,
-FOREIGN KEY (match_ID) REFERENCES ChessMatch(match_ID) ON DELETE CASCADE
+	tournament_ID INT,
+	match_ID INT NOT NULL,
+	 
+	PRIMARY KEY (tournament_ID, match_ID),
+	FOREIGN KEY (tournament_ID) REFERENCES Tournament(tournament_ID) ON DELETE CASCADE,
+	FOREIGN KEY (match_ID) REFERENCES ChessMatch(match_ID) ON DELETE CASCADE
 );
 
  
